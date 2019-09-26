2b4139524dbddad573cdb149030faff5811dd3e5
Nomost <guillaume.soufflet1@viacesi.fr>
2019-09-26 09:22:59 +0200

    client and todo functions

---
+++ src/serverless/todos-fn/handler.js
@@ -0,0 +1,21 @@
+"use strict"
+
+module.exports = async (config) => {
+    const app = config.app;
+
+    app.get('/', (req, res) => {
+
+    });
+
+    app.put('/', (req, res) => {
+
+    });
+
+    app.post('/', (req, res) => {
+
+    });
+
+    app.delete('/', (req, res) => {
+
+    });
+}
