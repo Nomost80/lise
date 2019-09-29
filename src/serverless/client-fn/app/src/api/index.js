096eab02fab2bf13ad90149db1916c7e725d6aaf
Guillaume Soufflet <guillaume.soufflet1@viacesi.fr>
2019-09-29 18:04:59 +0200

    axios config

--- src/serverless/client-fn/app/src/api/index.js
+++ src/serverless/client-fn/app/src/api/index.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const webservice = axios.create({
-    baseURL: 'http://192.168.1.221:31112/function'
+    baseURL: 'http://192.168.1.221:31112/function',
+    responseType: 'json'
+});
+
+webservice.interceptors.response.use(response => {
+    response.data
 });
 
 export const getTasks = () =>
